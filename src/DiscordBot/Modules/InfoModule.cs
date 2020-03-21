f5b98548cc825f729ab5f857ba63a062c665c5af
DisabledMallis <mallisiscool@gmail.com>
2020-03-21 04:22:55 -0400

    start message filter

--- src/DiscordBot/Modules/InfoModule.cs
+++ src/DiscordBot/Modules/InfoModule.cs
@@ -8,6 +8,6 @@ namespace DiscordBot.Modules
         [Command("info")]
         public Task Info()
             => ReplyAsync(
-                $"Hello, I am a bot called {Context.Client.CurrentUser.Username} written in Discord.Net 1.0\n");
+                $"Hello, World!");
     }
 }
---
+++ src/DiscordBot/Services/MessageFilterService.cs
@@ -0,0 +1,30 @@
+﻿using Discord.Commands;
+using Discord.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscordBot.Services
+{
+    public class MessageFilterService
+    {
+        private readonly DiscordSocketClient _discord;
+        private readonly CommandService _commands;
+        private IServiceProvider _provider;
+
+        public MessageFilterService(IServiceProvider provider, DiscordSocketClient discord, CommandService commands)
+        {
+            _discord = discord;
+            _commands = commands;
+            _provider = provider;
+
+            _discord.MessageReceived += MessageReceived;
+        }
+
+        private Task MessageReceived(SocketMessage arg)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
